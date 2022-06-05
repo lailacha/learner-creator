304dd84db0af219cb2c2031880ffa0cf3b22c7a2
Ricardo <abrahamricardogn224@gmail.com>
2022-06-05 10:52:33 +0200

    verification de l'existance du fichier config.inc.php afin de charger ou pas  (autoloder)

--- www/Controller/Main.class.php
+++ www/Controller/Main.class.php
@@ -9,6 +9,37 @@ class Main extends BaseController{
 
     public function home()
     {
+        
+        
+            // Get current directory
+    echo getcwd() . "<br>";
+
+    // Change directory
+    chdir("../");
+
+    // Get current directory
+    echo getcwd();
+
+    $dir = getcwd();
+
+    // Permet de savoir si le fichier existe
+    $filename = $dir.'/conf.inc.php';
+        if (file_exists($filename)) {
+            echo "Le fichier $filename existe.";
+        } else {
+            echo "Le fichier $filename n'existe pas.";
+        }
+
+    /* Permet d'afficher le contenu d'un dossier */
+    /* if (is_dir($dir)){
+        if ($dh = opendir($dir)) {
+          while (($file = readdir($dh)) !== false){
+            echo "filename:" . $file . "<br>";
+          }
+          closedir($dh);
+        }
+    } */
+
         echo "Welcome";
     }
 
--- www/Model/User.class.php
+++ www/Model/User.class.php
@@ -286,7 +286,7 @@ class User extends Sql
                 
             "csrf_token" => [
                     "placeholder" => $_SESSION['csrf_token'],
-                    "type" => "text",
+                    "type" => "csrf_token",
                     "id" => "pwdcsrf",
                     "class" => "formRegister",
                     "required" => true,

ed36b41e5e15330b5f61864e744203e8e6759f59
Ossama9 <oussama.dahbi98@gmail.com>
2022-07-11 23:14:29 +0200

    Fix bug register & conf.inc.php

--- www/Controller/Main.class.php
+++ www/Controller/Main.class.php
@@ -7,28 +7,26 @@ use App\Controller\BaseController;
 use App\Core\Session;
 use App\Model\User as UserModel;
 
-class Main extends BaseController{
+class Main extends BaseController
+{
 
     public function home()
     {
         $session = new Session();
         $user = new UserModel();
 
-    // Permet de savoir si le fichier existe
-    //$filename ='conf.inc.php';      
-    $filename ='conf.php';  
-    
-    if(file_exists($filename)){
-        echo "Le fichier $filename existe.";
+        // Permet de savoir si le fichier existe
+        //$filename ='conf.inc.php';
+        $filename = 'conf.php';
 
-        $view = new View("home", "home");
-        $session->addFlashMessage("success", "Le fichier s'est bien installé.");
-    }else {
-        echo "Le fichier $filename n'existe pas.";
-        $view = new View("installeur", "back");
-        $session->addFlashMessage("success", "Le fichier $filename n'existe pas.");
-        
-    }
+        if (file_exists($filename)) {
+            $view = new View("home", "home");
+            $session->addFlashMessage("success", "Le fichier s'est bien installé.");
+        } else {
+            $view = new View("installeur", "back");
+            $session->addFlashMessage("success", "Le fichier $filename n'existe pas.");
+
+        }
     }
 
 
@@ -42,66 +40,66 @@ class Main extends BaseController{
 
         $user = new UserModel();
 
-        $filename ='conf.php'; 
+        $filename = 'conf.php';
         // double securité verifier si le fichier conf existe, si il existe il faut rediriger vers l'index.php
-        if(file_exists($filename)){ 
+        if (file_exists($filename)) {
             echo "Vous avez déjà installer ";
-        }else{
+        } else {
             // Pour eviter de se faire hacker
             if (!empty($_GET)) {
-            $myfile = fopen("conf.inc.php", "w");
-            fwrite($myfile, "\n");
-            
+                $myfile = fopen("conf.inc.php", "w");
+                fwrite($myfile, "\n");
 
-            //echo("Super");
-            //var_dump($_GET);
 
-            $txt ="<?php";
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-        
-            $txt = 'define("MAIL_SENDER_NAME","'.$_GET["MAIL_SENDER_NAME"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("MAILJET_API_KEY","'.$_GET["MAILJET_API_KEY"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("MAIL_SENDER","'.$_GET["MAIL_SENDER"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("DBNAME","'.$_GET["DBNAME"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("DBDRIVER","'.$_GET["DBDRIVER"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("DBUSER","'.$_GET["DBUSER"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("DBPWD","'.$_GET["DBPWD"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("DBHOST","'.$_GET["DBHOST"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("DBPORT","'.$_GET["DBPORT"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("DBPREFIXE","'.$_GET["DBPREFIXE"].'");';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            $txt = 'define("MB",'.$_GET["MB"].');';
-            fwrite($myfile,$txt);
-            fwrite($myfile, "\n");
-            
-            $myfile = fopen("conf.php", "w");
-            $txt = "fichier temoins pour certifier de l'installation";
-            fwrite($myfile,$txt);
+                //echo("Super");
+                //var_dump($_GET);
+
+                $txt = "<?php";
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+
+                $txt = 'define("MAIL_SENDER_NAME","' . $_GET["MAIL_SENDER_NAME"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("MAILJET_API_KEY","' . $_GET["MAILJET_API_KEY"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("MAIL_SENDER","' . $_GET["MAIL_SENDER"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("DBNAME","' . $_GET["DBNAME"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("DBDRIVER","' . $_GET["DBDRIVER"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("DBUSER","' . $_GET["DBUSER"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("DBPWD","' . $_GET["DBPWD"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("DBHOST","' . $_GET["DBHOST"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("DBPORT","' . $_GET["DBPORT"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("DBPREFIXE","' . $_GET["DBPREFIXE"] . '");';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+                $txt = 'define("MB",' . $_GET["MB"] . ');';
+                fwrite($myfile, $txt);
+                fwrite($myfile, "\n");
+
+                $myfile = fopen("conf.php", "w");
+                $txt = "fichier temoins pour certifier de l'installation";
+                fwrite($myfile, $txt);
                 #echo($myfile);
                 echo("********************************");
                 echo("<br>");
                 echo("Bravo L'instalation est faite");
-                $user -> initdb();
+                $user->initdb();
             }
         }
     }
